1c8b3eacb2a2914a94c7f0148671e69bd75cff5b
bsdrgroup <bsdr111@gmail.com>
2013-07-29 15:41:37 -0500

    created a.p1.1.js
    created comments

---
+++ bjk/a.p1.1.js
@@ -0,0 +1,35 @@
+/*
+ ______   _        _______  _______ __________________ _______  _       
+(  ___ \ ( \      (  ___  )(       )\__    _/\__   __/(  ____ \| \    /\
+| (   ) )| (      | (   ) || () () |   )  (     ) (   | (    \/|  \  / /
+| (__/ / | |      | (___) || || || |   |  |     | |   | |      |  (_/ / 
+|  __ (  | |      |  ___  || |(_)| |   |  |     | |   | |      |   _ (  
+| (  \ \ | |      | (   ) || |   | |   |  |     | |   | |      |  ( \ \ 
+| )___) )| (____/\| )   ( || )   ( ||\_)  )  ___) (___| (____/\|  /  \ \
+|/ \___/ (_______/|/     \||/     \|(____/   \_______/(_______/|_/    \/
+
+This is bjk Alpha Prerelease 1.1 : the very first **actual** version of blamjick.
+
+Everything under bsdrgroup/blamjick is released under the GNU General Public License version 3.
+
+/*
+/*
+
+BLAMJICK vA.P1.1
+
+Copyright (C) 2013  the BSDR GRroup
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+    
+JavaScript is a trademark or registered trademark of Oracle in the U.S. and other countries.
+GitHub is © 2013 GitHub, Inc.
+
+/*
